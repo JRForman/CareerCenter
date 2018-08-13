cce2d9924c03747d1ba4d5608f75b3dadc6cd86c
Cerise <developerdanielle@gmail.com>
2018-08-13 13:03:07 -0700

    sql routes

--- routes/apiRoutes.js
+++ routes/apiRoutes.js
@@ -1,24 +1,29 @@
 var db = require("../models");
 
 module.exports = function(app) {
-  // Get all examples
-  app.get("/api/examples", function(req, res) {
-    db.Example.findAll({}).then(function(dbExamples) {
-      res.json(dbExamples);
-    });
+  app.get("/api/:Category?", function(req, res) {
+  //  get only category
+    if (req.params.Category) {
+      db.jobs.findAll({
+        where: {
+          Category: req.params.Category
+        }
+      }).then(function(dbjobs) {
+        return res.json(dbjobs);
+      });
+    }
+  });
+  app.get("/api/:AS_Code?", function(req, res) {
+    // get only salary 
+    if (req.params.AS_Code) {
+      db.jobs.findAll({
+        where: {
+          AS_Code: req.params.AS_Code
+        }
+      }).then(function(dbjobs) {
+        return res.json(dbjobs);
+      });
+    }
   });
 
-  // Create a new example
-  app.post("/api/examples", function(req, res) {
-    db.Example.create(req.body).then(function(dbExample) {
-      res.json(dbExample);
-    });
-  });
-
-  // Delete an example by id
-  app.delete("/api/examples/:id", function(req, res) {
-    db.Example.destroy({ where: { id: req.params.id } }).then(function(dbExample) {
-      res.json(dbExample);
-    });
-  });
 };
