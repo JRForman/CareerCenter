70691c6e0b63ca6567817b388f570c6568dd1c0d
Jason Forman <20210580+JRForman@users.noreply.github.com>
2018-08-15 14:22:41 -0700

    fixing

--- models/user.js
+++ models/user.js
@@ -5,15 +5,6 @@ var bcrypt = require("bcrypt-nodejs");
 module.exports = function(sequelize, DataTypes) {
   var User = sequelize.define("User", {
     // The email cannot be null, and must be a proper email before creation
-<<<<<<< Updated upstream
-    id: {
-      type: DataTypes.INTEGER,
-      allowNull: false,
-      unique: true,
-      primaryKey: true
-    },
-=======
->>>>>>> Stashed changes
     email: {
       type: DataTypes.STRING,
       allowNull: false,
