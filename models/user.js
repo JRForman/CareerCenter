43f13ea682eea3b0742529a57095d14e8b75c483
Alicia Collins <alicia.collins60@me.com>
2018-08-15 12:32:41 -0700

    added selections.js

--- models/user.js
+++ models/user.js
@@ -41,5 +41,12 @@ module.exports = function(sequelize, DataTypes) {
       null
     );
   });
+
+  User.associate = function(models) {
+    User.hasMany(models.Selected, {
+      onDelete: "cascade"
+    });
+  };
+
   return User;
 };
