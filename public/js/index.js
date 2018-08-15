5f7ae5ca32afa01adf170e16f3899113fa78d67d
Jason Forman <20210580+JRForman@users.noreply.github.com>
2018-08-15 07:16:12 -0700

    Results Displayed

--- public/js/index.js
+++ public/js/index.js
@@ -15,7 +15,6 @@ $(document).ready(function() {
   });
 
   $.get("/api/popAsCode").then(function(dbData) {
-    console.log(dbData);
     for (data in dbData) {
       $("#salary-search").append(
         "<option>" + dbData[data].asCode + "</option>"
@@ -23,8 +22,7 @@ $(document).ready(function() {
     }
   });
 
-  $.get("/api/popEducation").then(function(dbData) {
-    console.log(dbData);
+  $.get("/api/poptypicalEntryLevelEducation").then(function(dbData) {
     for (data in dbData) {
       $("#education-search").append(
         "<option>" + dbData[data].typicalEntryLevelEducation + "</option>"
@@ -41,7 +39,8 @@ $("#logout").on("click", function() {
 });
 
 // user subission click request
-$("#submit").on("click", function() {
+$("#submit").on("click", function(e) {
+  e.preventDefault();
   var searchedCategory = $("#category-search")
     .val()
     .trim();
@@ -53,51 +52,77 @@ $("#submit").on("click", function() {
     .trim();
   // get request from database
   $.get(
-    "/api/" + searchedCategory,
-    +searchedSalary,
-    +searchedEducation,
+    "/api/" + searchedCategory + "/" + searchedSalary + "/" + searchedEducation,
     function(dbData) {
       // log the data to our console
-      console.log(dbData);
       // empty to resultSection before adding new content
       $("#resultSection").empty();
       // if the data is not there, then return an error message
-      if (!dbData) {
+      if (!dbData[0]) {
         $("#resultSection").append(
           "<h2> No results found. Please change selections </h2>"
         );
       } else {
-        // otherwise
+        // otherwiseS
+        //add column headers
+        var head = $("<thead>");
+        var row = $("<tr>");
+        $(row).append("<th scope='col'>Add to Comparison</th>");
+        $(row).append("<th scope='col'>Occupation</th>");
+        $(row).append("<th scope='col'>Category</th>");
+        $(row).append("<th scope='col'>Median Annual Wage</th>");
+        $(row).append("<th scope='col'>Typcal Entry Level Eduction</th>");
+        $(row).append("<th scope='col'>Employment In 1000s (2016)</th>");
+        $(row).append(
+          "<th scope='col'>Employment Change 10 Year Projection</th>"
+        );
+        $(row).append(
+          "<th scope='col'>Work Experience In Related Occupation</th>"
+        );
+        $(row).append("<th scope='col'>Typical On The Job Training</th>");
+        $(head).append(row);
+        $("#resultSection").append(head);
+        $("#resultSection").append("<tbody>");
+
         // append the ocupation name
-        $("#resultSection").append(
-          "<td>" + dbData[data].occupationTitle + "</td>"
-        );
-        // the annual catagory
-        $("#resultSection").append("<td>" + dbData[data].catagory + "</td>");
-        // the annual wage
-        $("#resultSection").append(
-          "<td>" + dbData[data].MedianAnnualWage + "</td>"
-        );
-        // append the education requirement
-        $("#resultSection").append(
-          "<td>" + dbData[data].typicalEntryLevelEducation + "</td>"
-        );
-        // the employment level 2016
-        $("#resultSection").append(
-          "<td>" + dbData[data].employment2016Thousands + "</td>"
-        );
-        // the 10 yr employment change
-        $("#resultSection").append(
-          "<td>" + dbData[data].employmentChange20162026Percent + "</td>"
-        );
-        // append the work experience
-        $("#resultSection").append(
-          "<td>" + dbData[data].workExperienceInARelatedOccupation + "</td>"
-        );
-        // append the job training
-        $("#resultSection").append(
-          "<td>" + dbData[data].typicalOnheJobTraining + "</td>"
-        );
+        for (data in dbData) {
+          // console.log(dbData[data]);
+          var newRow = $("<tr>");
+          $(newRow).append(
+            "<td>" +
+              "<button type='button'>" +
+              "../images/iconmonstr-plus-circle-thin-24.png+" +
+              "</button>" +
+              "</td>"
+          );
+          $(newRow).append("<td>" + dbData[data].occupationTitle + "</td>");
+          // the annual catagory
+          $(newRow).append("<td>" + dbData[data].category + "</td>");
+          // the annual wage"
+          $(newRow).append("<td>" + dbData[data].medianAnnualWage + "</td>");
+          // append the education requirement
+          $(newRow).append(
+            "<td>" + dbData[data].typicalEntryLevelEducation + "</td>"
+          );
+          // the employment level 2016
+          $(newRow).append(
+            "<td>" + dbData[data].employment2016Thousands + "</td>"
+          );
+          // the 10 yr employment change
+          $(newRow).append(
+            "<td>" + dbData[data].employmentChange20162026Percent + "</td>"
+          );
+          // append the work experience
+          $(newRow).append(
+            "<td>" + dbData[data].workExperienceInARelatedOccupation + "</td>"
+          );
+          // append the job training
+          $(newRow).append(
+            "<td>" + dbData[data].TypicalOnTheJobTraining + "</td>"
+          );
+          // $("#resultSection").append(newRow);
+          $("#resultSection > tbody:last-child").append(newRow);
+        }
       }
     }
   );
